63a2b6dca8731468dfa5c97862e35364646a9dc1
Correct Name <26098287+MrMonk3y@users.noreply.github.com>
2018-12-10 00:49:41 +0100

    another code refactoring
    changed X#X prefix to @#X since deepl changed it sometimes to Xxx
    
    deepl makes out of:
    @#X-9001612251494444254
    @#X-900161225149444444254
    
    reason unclear, have to solve this issue somehow. deepl is unpredictable

--- texTrans.py
+++ texTrans.py
@@ -9,7 +9,6 @@ import re
 import pydeepl
 import argparse
 import time
-#from random import choice
 from tqdm import tqdm
 
 def parse_args(args=None):
@@ -18,7 +17,6 @@ def parse_args(args=None):
     parser.add_argument("-t", dest="TO", default="EN", required=True, help="Language of the target document e.g EN")
     parser.add_argument("-i", dest="FILENAME", required=True, nargs="+", help="Path(s) to the latex file(s)")
     return parser.parse_args(args)
-
 
 def make_xlat(*args, **kwds):
     adict = dict(*args, **kwds)
@@ -29,12 +27,29 @@ def make_xlat(*args, **kwds):
         return rx.sub(one_xlat, text)
     return xlat
 
+def translate(text: str, lang_in="DE", lang_out="EN"):
+    translated = []
+    commands = (r"^@#X\d{18,19}$", # if it starts with @#X followed by 18 to 19 digits its just a hash --> no translation needed
+                r"^@#X-\d{18,19}$")
+    only_hash_pattern = re.compile("|".join(commands))
+    for line in text.splitlines():
+        #print(line)
+        if line in {'', '\n'} or only_hash_pattern.match(line):
+            translated.append(line)
+#        elif not line.strip():
+#            translated.append('')
+        else:
+            translated.append(pydeepl.translate(line, lang_in, lang_out))
+            time.sleep(0.6) #problem with to many requests. not yet solved
+    translated = '\n'.join(translated)
+    return translated
+
 if __name__ == "__main__":
     args = parse_args()
-    print('Translating file {} to {} from {}'.format(args.FILENAME, args.TO, args.FROM))
+    print('Translating file {} to {} from {}'.format(*args.FILENAME, args.TO, args.FROM))
     fileInputName = args.FILENAME[0]
     #fileInputName = "Introduction.tex"
-    fileOutName = fileInputName.split('.')[0]+"_trans.tex"
+    fileOutName = fileInputName.split('.')[0] + "_trans.tex"
 
     with open(fileInputName) as fileIn, open(fileOutName, "w") as fileOut:
 
@@ -57,22 +72,22 @@ if __name__ == "__main__":
             r"\%.*",
         )
         search_result_1 = re.findall("|".join(search_pattern), fileStr)
-#        searchObj1 = re.findall( r"\\begin\{\w+\}|\t|    |\r|\\end\{\w+\}|\\usepackage\{\w+\}|\\newcommand\{\w+\}|\\include\{.*\}|\\input\{\w+\}|\\\w+\[.*\}|\%.*", fileStr)
-        #random number for every found command + a prefix which hopefully doens't appear in text. Used to skip lines later, which don't need translation
-        list1 = ['X#X{}'.format(hash(x)) for x in search_result_1]
+        # random number for every found command + a prefix which hopefully 
+        # doens't appear in text. Used to skip lines later, which don't need translation
+        list1 = ['@#X{}'.format(hash(x)) for x in search_result_1]
         #make a dictionary out of hashes
         d1 = dict(zip(search_result_1, list1))
-        translate = make_xlat(d1)
-        hashedText = translate(fileStr)
+        hash_dictionary = make_xlat(d1)
+        hashedText = hash_dictionary(fileStr)
     
         #replace all latex commands (starting with a backslash) with hashes
-        search_result_2 = re.findall( r"\\\w+",hashedText)
+        search_result_2 = re.findall( r"\\\w+", hashedText)
         #random number  + prefix again
-        list2 = ['X#X{}'.format(hash(x)) for x in search_result_2]
+        list2 = ['@#X{}'.format(hash(x)) for x in search_result_2]
         #make a dictionary
-        d2 = dict(zip(search_result_2,list2))
-        translate = make_xlat(d2)
-        hashedText = translate(hashedText)
+        d2 = dict(zip(search_result_2, list2))
+        hash_dictionary = make_xlat(d2)
+        hashedText = hash_dictionary(hashedText)
         #print(hashedText)
         #fileOut.write(translate(hashedText))
     
@@ -82,19 +97,7 @@ if __name__ == "__main__":
     
         print("Hashing done. Starting translation...")
 
-        translated = []
-        for line in hashedText.splitlines():
-            #print(line)
-            if line.startswith("X#X") and len(line) == 22:
-                translated.append(line)
-            elif not line.strip():
-                translated.append('')
-            else:
-                translated.append(pydeepl.translate(line, args.FROM, args.TO))
-                time.sleep(0.6) #problem with to many requests. not yet solved
-        translated = '\n'.join(translated)
-        #translated = translated+pydeepl.translate(hashedText, "DE", "EN")
-        #print(translated)
+        translated = translate(text=hashedText, lang_in = args.FROM, lang_out = args.TO)
     
         d1Inv = {val:key for (key, val) in d1.items()} #swap dictionary
         translate2 = make_xlat(d1Inv)
@@ -104,5 +107,3 @@ if __name__ == "__main__":
         fileOut.write(fileStrOut)
     
         print("Success")
-#        fileIn.close()
-#        fileOut.close()
